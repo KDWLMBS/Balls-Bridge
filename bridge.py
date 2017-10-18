f8ae4bd81b6686f6dce9021a13328bd18c55515b
Proxey <oberhofer.david@gmail.com>
2017-10-18 13:29:43 +0200

    changes

--- bridge.py
+++ bridge.py
@@ -5,6 +5,7 @@ from array import array
 import os
 import sys
 import time
+import thread
 import spidev
 import RPi.GPIO as GPIO
 #https://github.com/doceme/py-spidev
@@ -14,14 +15,12 @@ import random
 
 # PINS
 LASER_PIN=10
-CONTROLLER1_PIN=7
-CONTROLLER2_PIN=8
+CONTROLLER0_PIN=7
+CONTROLLER1_PIN=8
 
 # OTHER CONFIGS
-SPI_SPEED=3000000
+SPI_SPEED=500000
 MOTOR_COUNT=30
-
-
 
 
 #https://stackoverflow.com/questions/12173774/modify-bits-in-an-integer-in-python
@@ -42,15 +41,27 @@ class Controller:
         for i in range(motorCount):
             motor = Motor(i + motorStartIndex)
             self.motors.append(motor)
-            self.motors.randomPosition()
+            #motor.randomPosition()
+
+    def randomize_positions(self):
+        for motor in self.motors:
+            motor.randomize_position()
+
+    def drive(self):
+        data = 0xffffffff
+        for motor in self.motors:
+            data = motor.drive(data)
+        return data
+
 
 class Motor:
-    minPosition = -1000
-    maxPosition = 1000
+    minPosition = -400
+    maxPosition = 400
 
     def __init__(self, index):
         self.position = 0
         self.target = 0
+        # drive bit is inverted -> 0 = drive, 1 = don't drive
         self.driveBit = (index * 2)
         self.directionBit = (index * 2) + 1
         self.index = index
@@ -58,8 +69,10 @@ class Motor:
     # pass the 32-bit integer. we will set our 2 bytes
     def drive(self, data):
         if self.position == self.target:
-            data = set_bit(data, self.driveBit, 0)
+            # drive bit is inverted -> 0 = drive, 1 = don't drive
+            data = set_bit(data, self.driveBit, 1)
         else:
+            # drive bit is inverted -> 0 = drive, 1 = don't drive
             data = set_bit(data, self.driveBit, 1)
             if(self.target > self.position):
                 self.position += 1
@@ -69,22 +82,16 @@ class Motor:
                 data = set_bit(data, self.directionBit, 0)
         return data
 
-    def randomPosition(self):
+    def randomize_position(self):
         self.target = random.randrange(self.minPosition, self.maxPosition)
 
-motors0 = []
-motors1 = []
 
-for i in range(MOTOR_COUNT / 2):
-    motor = Motor(i)
-    motors0.append(motor)
-    motors0.randomPosition()
-    data0 = motor.drive(data0)
-
+controller0 = Controller('0', 15, 0)
+controller1 = Controller('0', 15, 0)
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(CONTROLLER0_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(CONTROLLER1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(CONTROLLER2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(LASER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 spi = spidev.SpiDev()
@@ -106,33 +113,56 @@ def index():
 @app.route('/send', methods=['POST'])
 def send():
   data = request.json['data']
-  #TODO send data to controller
+  #TODO send data to controller;
   return "ok"
 
 def send_positions(x):
-    global data
-    print "writing " + str(bin(data))
+    spi.writebytes([0x12, 0x34, 0x56, 0x67])
+    return
+    if x == CONTROLLER0_PIN:
+        print "interrupted0"
+        controller = controller0
+    elif x == CONTROLLER1_PIN:
+        print "interrupted1"
+        controller = controller1
+    else:
+        print "cannot send position. controller " + str(x) + " unknown"
+        return
+
+    data = controller.drive()
     splitted = struct.unpack('4B', struct.pack('>I', data))
-    for numb in splitted:
-        print hex(numb)
     spi.writebytes(splitted)
 
 def laser_interrupted(x):
     print "omg laser interrupted"
 
-GPIO.add_event_detect(CONTROLLER1_PIN, GPIO.RISING, callback=send_positions, bouncetime=300)
-GPIO.add_event_detect(CONTROLLER2_PIN, GPIO.RISING, callback=send_positions, bouncetime=300)
-GPIO.add_event_detect(LASER_PIN, GPIO.RISING, callback=laser_interrupted, bouncetime=300)
+def randomize_positions_thread():
+    while 1:
+        print "randomizing the positions"
+        controller0.randomize_positions()
+        print bin(controller0.drive())
+        time.sleep(5)
+
+
+
+GPIO.add_event_detect(CONTROLLER0_PIN, GPIO.RISING, callback=send_positions, )
+#GPIO.add_event_detect(CONTROLLER1_PIN, GPIO.RISING, callback=send_positions)
+#GPIO.add_event_detect(LASER_PIN, GPIO.RISING, callback=laser_interrupted)
 
 try:
     if __name__ == '__main__':
+        #thread.start_new_thread(randomize_positions_thread, ())
+        #thread.start_new_thread(wait_for_interrupt, ())
+        while 1:
+            pass
         # todo: change this to 127.0.0.1 for production
-        app.run(port=port,host="0.0.0.0")
+        # app.run(port=port,host="0.0.0.0")
 except KeyboardInterrupt:
     print "cleanup after KeyboardInterrupt"
     GPIO.cleanup()
     spi.close()
-except:
+except BaseException as e:
+    print e
     print "cleanup after except"
     GPIO.cleanup()
     spi.close()
